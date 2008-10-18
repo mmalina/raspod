3d8e52d7c0bf2368add289d4d534469cdc7c0a78
anyberry <anyberry@f51ce1a4-9d2c-11dd-8bdc-dd70ed116f32>
2008-10-18 19:52:03 +0000

    Added a simple installator
    git-svn-id: http://raspod.googlecode.com/svn/trunk@9 f51ce1a4-9d2c-11dd-8bdc-dd70ed116f32

---
+++ install/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Go to the directory of this script
+cd `dirname $0`
+
+#Install the binary into ~/bin/
+echo "Installing raspod into ~/bin/"
+if [ -f ~/bin/raspod ]
+then
+	echo "~/bin/raspod already exists, overriding..."
+fi
+cp ../bin/raspod ~/bin/
+
+#Install the basic conf file if not present
+echo "Installing raspod.conf into ~/.raspod/"
+if [ -f ~/.raspod/raspod.conf ]
+then
+	echo "~/raspod/raspod.conf already exists, omitting..."
+else
+	cp ../conf/raspod.conf ~/.raspod/
+fi
